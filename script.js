40ac4c123c8747c8a8f1cbf199e18a054ba16359
Roky777 <paulroky78@gmail.com>
2026-09-08 12:30:04 +0530

    Add Grade 3 How Things Work variant

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ let gameContent = null;
 // --- Progress Save System Integration ---
 let gameManager = null;
 let highestLevelPlayed = 1; // Default to level 1
-const MAX_GAME_LEVEL = 2;
+const MAX_GAME_LEVEL = 1;
 const TUTORIAL_STORAGE_KEY = "brainmatch_shape_friends_tutorial_seen";
 
 // Load game content from JSON file
@@ -707,9 +707,9 @@ function handleCampaignWin() {
 }
 
 function calculateFinalStars(totalXP) {
-  if (totalXP >= 100) {
+  if (totalXP >= 40) {
     return 3;
-  } else if (totalXP >= 60) {
+  } else if (totalXP >= 30) {
     return 2;
   } else {
     return 1; // 1 star for scores below 70
